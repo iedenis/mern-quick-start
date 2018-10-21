00ebd62dad6ed16111c53086f782a46739024276
iedenis <denis.ievlev@gmail.com>
2018-10-21 21:27:52 +0300

    added bootstrap dependency

--- server.js
+++ server.js
@@ -23,8 +23,12 @@ app.use(express.static(path.join(__dirname, './views')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.use('/', router);
+
 
 app.listen(port, () => {
     console.log('server started - ', port);
