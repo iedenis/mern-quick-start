37d5b6f2e644a50048565b32b860913480dec4af
iedenis <denis.ievlev@gmail.com>
2018-12-16 04:19:53 +0200

    security problem fixed

--- models/cars.model.js
+++ models/cars.model.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 const CarSchema = mongoose.Schema(
+   // _id: mongoose.Schema.Types.ObjectId,
+
     { name: { type: String, required: true, unique: true, index: true } },
     { collection: 'Car' });
 
